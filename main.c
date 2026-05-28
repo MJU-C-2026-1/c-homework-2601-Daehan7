7ba7d29a111cc4aa0955bff6c3fb7f15072e16c8
Daehan7 <jjds0427@gmail.com>
2026-05-28 15:01:13 +0000

    장대한-60261701-3차과제

--- main.c
+++ main.c
@@ -11,6 +11,46 @@ double bmicalculate(double weight, double height_m)
     return weight / (height_m * height_m);
 }
 
+double bmrcalculate(double weight,double height,int age)
+{
+  return 10 * weight + 6.25 * height - 5 * age;
+}
+
+void printfinal(char name[], char gender, int age)
+{
+  printf("\n--- %s님의 분석 결과입니다. ---\n\n", name);
+  printf("성별: %c / 나이: %d\n", gender, age);
+}
+
+void printbmi(double bmi)
+{
+  printf("현재 BMI 지수: %.2f\n, bmi");
+}
+
+double genderbmr(char gender, double bmr)
+{
+  if(gender == 'M')
+  {
+    bmr = bmr + 5;
+  }
+  else if(gender =='W')
+  {
+    bmr = bmr - 161;
+  }
+  else
+  {
+    printf("잘못된 성별을 입력하셨습니다. BMR 값을 0으로 처리합니다.");
+    bmr = 0;
+  }
+
+  return bmr;
+}
+
+void printbmr(double bmr)
+{
+  printf("현재 예상 BMR(기초대사량) 지수: %.2fkcal\n", bmr);
+}
+
 int main() {
 
   char name[20]; //이용자의 이름
@@ -28,58 +68,67 @@ int main() {
   scanf("%d %lf %lf", &age, &height, &weight);
 
   printf("무엇을 하고 싶으신가요? :\n");
-  printf("1. BMI 계산\t2. BMR(기초대사량) 계산\t3. 종료)");
+  printf("1. BMI 계산\t2. BMR(기초대사량) 계산\t3. BMI,BMR(기초대사량) 동시 계산\t 4.종료)");
   scanf("%d", &choice);
+
+  double height_m;
+  height_m = height / 100.0;
+
+  if(age <= 0 || height <= 0 || weight <= 0)
+  {
+    printf("비정상적인 수치가 감지되어 프로그램이 종료됩니다.");
+    return 0;
+  }
+
 
   switch(choice)
   {
     case 1:
       printf("BMI 계산을 선택하였습니다.");
-      bmicalculate() = bmi;
+      bmi = bmicalculate(weight, height_m);
+      printfinal(name, gender, age);
+      printbmi(bmi);
+      if(bmi >= 25.0)
+      {
+        printf("\n 표준 BMI지수보다 수치가 높습니다. 약간의 운동을 추천합니다. \n");
+      }
+      break;
+    
+    case 2:
+      printf("BMR(기초대사량) 계산을 선택하였습니다.");
+      bmr = bmrcalculate(weight, height, age);
+      bmr = genderbmr(gender, bmr);
+      printfinal(name, gender, age);
+      printbmr(bmr);
+      break;
+
+    case 3:
+      printf("BMI, BMR(기초대사량) 동시 계산을 선택하셨습니다.");
+      bmi = bmicalculate(weight, height_m);
+      bmr = bmrcalculate(weight, height, age);
+      bmr = genderbmr(gender, bmr);
+      printfinal(name, gender, age);
+      printbmi(bmi);
+      if(bmi >= 25.0)
+      {
+        printf("\n 표준 BMI지수보다 수치가 높습니다. 약간의 운동을 추천합니다. \n");
+      }
+      printbmr(bmr);
+      break;
+
+
+    case 4:
+      printf("프로그램을 종료합니다.");
+      break;
+
+    default:
+      printf("올바르지 않은 번호입니다. 프로그램을 종료합니다.");
+      break;
+
+
     
     
   }
-  
-
-  if( age <= 0 || height <= 0 || weight <= 0) //나이, 키, 몸무게에 비정상적인 수치가 작성됐는지 감지
-  {
-    printf("비정상적인 수치가 감지되어 프로그램이 종료됩니다.");
-    return 0;
-  }
-  
-  double height_m;
-  height_m = height / 100.0;
-  bmi = weight / (height_m * height_m);
-  bmr = 10 * weight + 6.25 * height - 5 * age;
-
-  if(gender == 'M')
-  {
-    bmr = bmr + 5;
-    if(bmi >= 25.0)
-    {
-      printf("\n 표준 BMI지수보다 수치가 높습니다. 약간의 운동을 추천합니다!\n");
-    }
-  }
-
-  else if(gender == 'W')
-  {
-    bmr = bmr - 161;
-    if(bmi >= 25.0)
-    {
-      printf("\n 표준 BMI지수보다 수치가 높습니다. 약간의 운동을 추천합니다!\n");
-    }
-  }
-  
-  else
-  {
-    printf("잘못된 성별을 입력하셨습니다. BMR 계산 결과는 분석 결과에서 제외됩니다.");
-    bmr = 0;
-  }
-
-  printf("\n--- %s님의 분석 결과입니다. ---\n\n", name);
-  printf("성별: %c / 나이 %d\n", gender, age);
-  printf("현재 BMI 지수: %.2f\n", bmi);
-  printf("현재 예상 기초 대사량(BMR): %.2f kcal\n", bmr);
 
   return 0;
 }
